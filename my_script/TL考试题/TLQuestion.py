61db6e3e81b783d9da3002e06320e74aa46e8a3f
liliangce <2656878153@qq.com>
2017-11-09 22:22:03 +0800

    mm

--- my_script/TL考试题/TLQuestion.py
+++ my_script/TL考试题/TLQuestion.py
@@ -1,34 +1,64 @@
 # /usr/bin/evn python
 # coding:utf-8
 
+
+"""
+假设有5本组合一套，剩余的有3本组合一套，那么该种组合的方式需要转换为两种4本组合
+"""
 DISCOUNT_SELECTOR = {1: 1, 2: 0.95, 3: 0.9, 4: 0.80, 5: 0.75}
 
 
-class TLQuestion(object):
+class TotalPrice(object):
     """价格计算器"""
     def __init__(self, book_one=0, book_two=0, book_three=0, book_four=0, book_five=0):
-        self.buy_modle = [book_one, book_two, book_three, book_four, book_five]
+        self.buy_books = [book_one, book_two, book_three, book_four, book_five]
 
-    def lowest_price(self):
-        """计算最低价格"""
-        buy_modle = self.buy_modle
-        price = 0
-        book_type = len(buy_modle)
+    def decomposition_books(self):
+        """由多到少最大组合"""
+        buy_books = self.buy_books
+        book_type = len(buy_books)
+        composition = {}
         for i in range(book_type):
-            buy_modle.sort(reverse=True)
-            rest_book_type = len(buy_modle)
+            buy_books.sort(reverse=True)
+            rest_book_type = len(buy_books)
             for j in range(rest_book_type):
-                buy_modle.sort(reverse=True)
-                if buy_modle[-1] == 0:
-                    buy_modle.pop()
-            rest_book_type = len(buy_modle)
+                buy_books.sort(reverse=True)
+                if buy_books[-1] == 0:
+                    buy_books.pop()
+            rest_book_type = len(buy_books)
             if rest_book_type:
-                min_num = buy_modle[-1]
-                price += min_num * (rest_book_type * 8.0 * DISCOUNT_SELECTOR.get(rest_book_type))
+                composition[rest_book_type] = buy_books[-1]
                 for k in range(rest_book_type):
-                    buy_modle[k] -= min_num
+                    buy_books[k] -= buy_books[-1]
+        return composition
+
+    def compute_best_composition(self):
+        """如果有五本组合和3本组合存在，则需要转换最优组合"""
+        best_composition = self.decomposition_books()
+        if 5 in best_composition.keys() and 3 in best_composition.keys():
+            if best_composition[3] > best_composition[5]:
+                if 4 not in best_composition.keys():
+                    best_composition[4] = 0
+                best_composition[4] += best_composition[5]*2
+                best_composition[3] -= best_composition[5]
+                best_composition.pop(5)
+            else:
+                if 4 not in best_composition.keys():
+                    best_composition[4] = 0
+                best_composition[4] += best_composition[3]*2
+                best_composition[5] -= best_composition[3]
+                best_composition.pop(3)
+        print best_composition
+        return best_composition
+
+    def total_price(self):
+        """计算最低价格"""
+        best_composition = self.compute_best_composition()
+        price = 0
+        for i in best_composition.keys():
+            price += best_composition[i]*i*8*DISCOUNT_SELECTOR[i]
         return price
 
 if __name__ == "__main__":
-    test = TLQuestion(3,2,1)
-    print test.lowest_price()
+    test = TotalPrice(2, 2, 2, 1, 1)
+    print test.total_price()
--- my_script/TL考试题/TestTLQuestion.py
+++ my_script/TL考试题/TestTLQuestion.py
@@ -2,7 +2,7 @@
 # coding:utf-8
 
 import unittest
-from TLQuestion import *
+from TLQuestion import TotalPrice
 
 
 class TestTLQuestion(unittest.TestCase):
@@ -14,13 +14,14 @@ class TestTLQuestion(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_lowest_price_1(self):
-        self.assertEqual(TLQuestion(2,2,2,1,1).lowest_price(), 51.6, 'test lowest_price fail')
+    def test_total_price_1(self):
+        self.assertEqual(TotalPrice(2, 2, 2, 1, 1).total_price(), 51.2, 'test total_price fail')
 
-    def test_lowest_price_2(self):
-        self.assertEqual(TLQuestion(book_one=1, book_two=2).lowest_price(), 23.2, 'test lowest_price fail')
+    def test_total_price_2(self):
+        self.assertEqual(TotalPrice(1, 2).total_price(), 23.2, 'test total_price fail')
 
-    def test_lowest_price_3(self):
-        self.assertEqual(TLQuestion(book_one=3, book_two=1, book_three=2).lowest_price(), 44.8, 'test lowest_price fail')
+    def test_total_price_3(self):
+        self.assertEqual(TotalPrice(3, 1, 2).total_price(), 44.8, 'test total_price fail')
+
 if __name__=="__main__":
     unittest.main()
